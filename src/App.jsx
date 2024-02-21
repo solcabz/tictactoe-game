8e0554795c85bbc3b6c1fcbd5fa777fcbac6ae40
solcabz <xhzoul30@gmail.com>
2024-02-21 15:27:54 +0800

    state to app component show status message  calculate the winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import './styles.scss';
+import { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
+  //state where the square box is value empty
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(false);
+
+  //calculates winner tru winner.js
+  const winner = calculateWinner(squares);
+
+  //provide message who's turn X or O
+  const playerTurn = isXNext ? 'X' : 'O';
+
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${playerTurn}`;
+
+  const handleSquareClick = clickPosition => {
+    //condition if the square box has valued it stayed as its current state
+    if (squares[clickPosition] || winner) {
+      return;
+    }
+
+    //function if the square box is null it place the value of O or X vice versa
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    //function if the square box is null it place the value of O or X as its next move
+    setXNext(currentXNext => !currentXNext);
+  };
+
   return (
     <>
       <div className="app">
-        <Board></Board>
+        <h2>{statusMessage} </h2>
+        <Board squares={squares} handleSquareClick={handleSquareClick} />
       </div>
     </>
   );
