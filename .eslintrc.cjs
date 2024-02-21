5daef8e7057232da4343de231c91ea426c8122fc
solcabz <xhzoul30@gmail.com>
2024-02-21 13:43:35 +0800

    no-unused-vars warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
